e5f241e1d5219b9b61aabd9ada6d26f807dfa1eb
Leyen Qian <leyenqian@gmail.com>
2020-12-03 17:15:28 -0700

    nothing new

--- iocp.h
+++ iocp.h
@@ -1,21 +1,3 @@
-/*+===================================================================
-  File:      iocp.h
-
-  Summary:   Brief summary of the file contents and purpose.
-
-  Classes:   Classes declared or used (in source files).
-
-  Functions: Functions exported (in source files).
-
-  Origin:    Indications of where content may have come from. This
-             is not a change history but rather a reference to the
-             editor-inheritance behind the content or other
-             indications about the origin of the source.
-
-  Copyright and Legal notices.
-  Copyright and Legal notices.
-===================================================================+*/
-
 #include "pre_define.h"
 #include "link_pool.h"
 #include "redis_connector.h"
@@ -28,8 +10,6 @@ private:
     PPER_LINK_INFO p_ser_link_info = NULL;
     LinkPool link_pool;
     
-
-    // dynamically load functions
     LPFN_ACCEPTEX p_AcceptEx = NULL;
     LPFN_DISCONNECTEX p_DisconnectEx = NULL;
     LPFN_GETACCEPTEXSOCKADDRS p_GetAcceptExSockAddrs = NULL;
