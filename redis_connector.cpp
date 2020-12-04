ef930f3d352f38c0254fdd588d4f2c9907dcf3b2
Leyen Qian <leyenqian@gmail.com>
2020-12-03 17:14:47 -0700

    add methods description

--- redis_connector.cpp
+++ redis_connector.cpp
@@ -1,20 +1,3 @@
-/*+===================================================================
-  File:      redis_connector.cpp
-
-  Summary:   Brief summary of the file contents and purpose.
-
-  Classes:   Classes declared or used (in source files).
-
-  Functions: Functions exported (in source files).
-
-  Origin:    Indications of where content may have come from. This
-             is not a change history but rather a reference to the
-             editor-inheritance behind the content or other
-             indications about the origin of the source.
-
-  Copyright and Legal notices.
-  Copyright and Legal notices.
-===================================================================+*/
 #include "redis_connector.h"
 
 /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
@@ -49,18 +32,10 @@ RedisConnector::~RedisConnector()
 /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
   Method:   RedisConnector::Connect
 
-  Summary:  Short summary of purpose and content of MyMethodOne.
-            Short summary of purpose and content of MyMethodOne.
+  Summary:  build non-secure connection to the redis server
 
-  Args:     MYTYPE MyArgOne
-              Short description of argument MyArgOne.
-            MYTYPE MyArgTwo
-              Short description of argument MyArgTwo.
-
-  Modifies: [list of member data variables modified by this method].
-
-  Returns:  MYRETURNTYPE
-              Short description of meaning of the return type values.
+  Returns:  INT
+              operation status
 M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 INT RedisConnector::Connect()
 {
@@ -86,18 +61,16 @@ INT RedisConnector::Connect()
 /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
   Method:   RedisConnector::ExecuteCommand
 
-  Summary:  Short summary of purpose and content of MyMethodOne.
-            Short summary of purpose and content of MyMethodOne.
+  Summary:  synchronously send command to the redis server for execution
+            then receive operation results
 
-  Args:     MYTYPE MyArgOne
-              Short description of argument MyArgOne.
-            MYTYPE MyArgTwo
-              Short description of argument MyArgTwo.
+  Args:     string command
+              command for execution
 
-  Modifies: [list of member data variables modified by this method].
+  Modifies: [p_redis_reply]
 
-  Returns:  MYRETURNTYPE
-              Short description of meaning of the return type values.
+  Returns:  INT
+              operation status
 M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 INT RedisConnector::ExecuteCommand(const string command)
 {
@@ -112,6 +85,10 @@ INT RedisConnector::ExecuteCommand(const string command)
   Method:   RedisConnector::TestRedis
 
   Summary:  Test basic redis command, adapted from hiredis/example/example.c
+
+  Modifies: [p_redis_reply]
+
+  Returns:  VOID
 M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 VOID RedisConnector::TestRedis()
 {
